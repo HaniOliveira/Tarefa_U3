9859e4255839482409ec61f7506c1e9f5f65d109
Haniel Oliveira <hanielpedreira@gmail.com>
2024-12-18 11:42:16 -0300

    Correção

--- Tarefa_U3.c
+++ Tarefa_U3.c
@@ -13,6 +13,7 @@ void converterArea();
 int main()
 {
     int escolha;
+
     do
     {
         printf("Selecione uma categoria para converter:\n");
@@ -71,53 +72,14 @@ void converterMassa()
 
 void converterVolume()
 {
-
-    int escolhaVolume;
-    printf("Selecione uma unidade de volume para converter:\n");
-    printf("1. Litro para Mililitro\n");
-    printf("2. Mililitro para Litro\n");
-    printf("3. Litro para Metro Cúbico\n");
-    printf("Digite sua escolha: ");
-    scanf("%d", &escolhaVolume);
-
-    float ml = 0;
-    float litro = 0;
-    float metroCubico = 0;
-    switch (escolhaVolume)
-    {
-    case 1:
-        printf("Digite o valor em litros: ");
-        scanf("%f", &litro);
-        ml = litro * 1000;
-        printf("O valor em mililitros é: %.2f\n", ml);
-        break;
-
-    case 2:
-        printf("Digite o valor em mililitros: ");
-        scanf("%f", &ml);
-        litro = ml / 1000;
-        printf("O valor em litros é: %.2f\n", litro);
-        break;
-
-    case 3:
-        printf("Digite o valor em litros: ");
-        scanf("%f", &litro);
-        metroCubico = litro / 1000;
-        printf("O valor em metros cúbicos é: %.2f\n", metroCubico);
-        break;
-
-    default:
-        printf("Opção inválida. Tente novamente.\n");
-        break;
-        break;
-    }
 }
 
 void converterTemperatura()
 {
 }
 
-void converterVelocidade() {
+void converterVelocidade()
+{
     int opcao;
     float velocidade, resultado;
 
@@ -133,34 +95,35 @@ void converterVelocidade() {
 
     printf("Digite a velocidade: ");
     scanf("%f", &velocidade);
-    
-        switch (opcao) {
-                case 1: // km/h para m/s
-                    resultado = velocidade / 3.6;
-                    printf("%.2f km/h equivalem a %.2f m/s\n", velocidade, resultado);
-                    break;
-                case 2: // km/h para mph
-                    resultado = velocidade / 1.609;
-                    printf("%.2f km/h equivalem a %.2f mph\n", velocidade, resultado);
-                    break;
-                case 3: // m/s para km/h
-                    resultado = velocidade * 3.6;
-                    printf("%.2f m/s equivalem a %.2f km/h\n", velocidade, resultado);
-                    break;
-                 case 4: // m/s para mph
-                    resultado = velocidade * 2.237;
-                    printf("%.2f m/s equivalem a %.2f mph\n", velocidade, resultado);
-                    break;
-                case 5: // mph para km/h
-                    resultado = velocidade * 1.609;
-                    printf("%.2f mph equivalem a %.2f km/h\n", velocidade, resultado);
-                    break;
-                case 6: // mph para m/s
-                    resultado = velocidade / 2.237;
-                    printf("%.2f mph equivalem a %.2f m/s\n", velocidade, resultado);
-                    break;
-                default:
-                    printf("Opção inválida!\n");
+
+    switch (opcao)
+    {
+    case 1: // km/h para m/s
+        resultado = velocidade / 3.6;
+        printf("%.2f km/h equivalem a %.2f m/s\n", velocidade, resultado);
+        break;
+    case 2: // km/h para mph
+        resultado = velocidade / 1.609;
+        printf("%.2f km/h equivalem a %.2f mph\n", velocidade, resultado);
+        break;
+    case 3: // m/s para km/h
+        resultado = velocidade * 3.6;
+        printf("%.2f m/s equivalem a %.2f km/h\n", velocidade, resultado);
+        break;
+    case 4: // m/s para mph
+        resultado = velocidade * 2.237;
+        printf("%.2f m/s equivalem a %.2f mph\n", velocidade, resultado);
+        break;
+    case 5: // mph para km/h
+        resultado = velocidade * 1.609;
+        printf("%.2f mph equivalem a %.2f km/h\n", velocidade, resultado);
+        break;
+    case 6: // mph para m/s
+        resultado = velocidade / 2.237;
+        printf("%.2f mph equivalem a %.2f m/s\n", velocidade, resultado);
+        break;
+    default:
+        printf("Opção inválida!\n");
     }
 }
 
